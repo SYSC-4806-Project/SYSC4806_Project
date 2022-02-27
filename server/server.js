05212e1241235d9686e28f53bf862e06f1842cec
Em-kale <davantzis@MSI.localdomain>
2022-02-27 18:55:21 -0500

    added dtabase configuration code

--- server/server.js
+++ server/server.js
@@ -1,9 +1,32 @@
-
+require("dotenv").config()
 const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
+
+
+
+const { MongoClient } = require("mongodb");
+const uri = process.env.MONGODB_CONNECTION_STRING ; // replace with url string for local developlment
+
+const client = new MongoClient(uri);
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db('sysc4806');
+      const surveys = database.collection('surveys');
+      // Query for a survey with test field that has content "test"
+      const query = { test: 'test' };
+      const survey = await surveys.findOne(query);
+      console.log(survey);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+run().catch(console.dir);
+
 
 app.use(express.static(publicPath));
 

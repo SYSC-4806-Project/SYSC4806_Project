f8b734581716145fb723cf8a7223b40d87174b1e
Em-kale <davantzis@MSI.localdomain>
2022-02-27 17:35:08 -0500

    Initial Files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        SYSC 4806 Project
+      </header>
+    </div>
+  );
+}
+
+export default App;

094b1e8dda80c355aae7e9079f7b6f888ce4063f
Em-kale <davantzis@MSI.localdomain>
2022-02-27 19:41:28 -0500

    More Server Config

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        SYSC4806 PROJECT
+      </header>
+    </div>
+  );
+}
+
+export default App;

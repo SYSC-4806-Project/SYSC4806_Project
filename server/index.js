e228d33a3258539859bcbda3d44c622028a70722
Emmitt Luhning <49374241+Em-kale@users.noreply.github.com>
2022-03-05 01:15:39 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.get("/testDatabase/", async (req, res) => {
 
 //return the react application
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
 });
  
 app.listen(port, () => {

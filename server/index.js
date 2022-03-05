f09b2e5e4b04cef46617d143fb9b81ac534fa400
Emmitt Luhning <49374241+Em-kale@users.noreply.github.com>
2022-03-05 00:31:31 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const publicPath = path.join(__dirname, '..', 'public');
 
  

e54374cbbef7dabc59b6e279ca3dc592fc28e72f
markocodes <Marko.Majkic@outlook.com>
2022-03-09 20:45:57 -0500

    Added post methods for adding blank surveys and survey responses to the database

--- server/index.js
+++ server/index.js
@@ -4,13 +4,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const publicPath = path.join(__dirname, '..', 'public');
+const bodyParser = require("body-parser");
 
  
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+// Configure express to use body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const { MongoClient } = require("mongodb");
+
+// const { MongoClient } = require("mongodb");
+const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.MONGODB_CONNECTION_STRING ; // replace with url string for local developlment
 const client = new MongoClient(uri);
 
@@ -46,3 +52,37 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
    console.log(`Server is up on port ${port}!`);
 });
+
+// POST survey responses to database
+app.post("/addResponses", async (req, res) => {
+  MongoClient.connect(uri, function (err, db) {
+    if (err)
+      throw err;
+    var dbo = db.db("test_surveys");
+    dbo.collection("survey_responses").insertOne(req.body,
+      function (err, result) {
+        if (err)
+          throw err;
+        res.json(result);
+        db.close();
+      });
+  });
+});
+
+// POST newly created survey to database
+app.post("/addSurvey", async (req, res) => {
+  MongoClient.connect(uri, function (err, db) {
+    if (err)
+      throw err;
+    var dbo = db.db("test_surveys");
+    dbo.collection("surveys").insertOne(req.body,
+      function (err, result) {
+        if (err)
+          throw err;
+        res.json(result);
+        db.close();
+      });
+  });
+});
+
+
